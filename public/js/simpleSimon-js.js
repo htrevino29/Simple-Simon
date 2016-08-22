e4d21ee83d04d4a754a9b2cc8267b5f74e3e2ce3
htrevino29 <htrevino29@gmail.com>
2016-08-22 14:11:57 -0500

    added iffy

--- public/js/simpleSimon-js.js
+++ public/js/simpleSimon-js.js
@@ -1,75 +1,73 @@
 console.log("linked");
-// Randomly select a square and fade that color in then out.
-var divColors = ["#red", "#green", "#yellow", "#blue"]; //# grabs id and lets the function change the css all in one go
-var randomDiv;
-var clickedDiv = '';
-var thisId;
-var randomSequence =[];
+(function(){
+	// Randomly select a square and fade that color in then out.
+	var divColors = ["#red", "#green", "#yellow", "#blue"]; 
+	//# grabs id and lets the function change the css all in one go
+	var randomDiv;
+	var clickedDiv = '';
+	var thisId;
+	var randomSequence =[];
 
 
 
-function simonsPick(){
-	var randomNum = Math.floor(Math.random()*divColors.length);
-	randomDiv = divColors[randomNum];
-	randomSequence.push(randomDiv);
-}
+	function simonsPick(){
+		var randomNum = Math.floor(Math.random()*divColors.length);
+		randomDiv = divColors[randomNum];
+		randomSequence.push(randomDiv);
+	}
 
-function flashColor(idSelector){   
-  $(idSelector).animate({
-  	opacity:'1'
-  	},200).animate({
-  	opacity:'0.5'
-  	},800)
-  	// console.log(idSelector);
-};
+	function flashColor(idSelector){   
+	  $(idSelector).animate({
+	  	opacity:'1'
+	  	},200).animate({
+	  	opacity:'0.5'
+	  	},800)
+	  	// console.log(idSelector);
+	};
 
-function playBack(){
-	var i = 0;
+	function playBack(){
+		var i = 0;
 
-	setInterval(function(){
-		var currentDiv = randomSequence[i];
-		flashColor(currentDiv); 
-		i++;
+		setInterval(function(){
+			var currentDiv = randomSequence[i];
+			flashColor(currentDiv); 
+			i++;
+		},1000);
+	}
 
-
-	},1000);
-}
-
-$( "#start" ).click(function() {
-	simonsPick(); 
-	playBack();
-	console.log(randomSequence);	
-// Allow the user to click on the square that was selected.
-
-	$( ".color" ).click(function() {
-
-		thisId = "#" + this.id;	
-		// console.log(thisId);
-		flashColor(thisId);
-		
-
-		
-		incorrect
-		correct
-		is there more
-
-		if (randomSequence[0] == thisId) {
-		console.log('you lose!') 
-		simonsPick();
+	$( "#start" ).click(function() {
+		simonsPick(); 
 		playBack();
-		} else if{
+		console.log(randomSequence);	
+	// Allow the user to click on the square that was selected.
 
-		}else{
+		$( ".color" ).click(function() {
+			thisId = "#" + this.id;	
+			// console.log(thisId);
+			flashColor(thisId);
+				
+			// incorrect
+			// correct
+			// is there more
 
-		}
+			if (randomSequence[0] !== thisId) {
+			console.log('you lose!'); 
+			simonsPick();
+			playBack();
+			} else if {
 
+			}else{
+
+			}
+
+		});
+	/////////////////////
+
+
+	/////////////////////	
 	});
-/////////////////////
 
-
-/////////////////////	
-});
-
+})();
 
 // Continue randomly selecting colored square/shapes 
 // adding the new random selection to be added to the previous selection. 
